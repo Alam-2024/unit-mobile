60a73388f9ba5fbb2e48f2bc31659fd8f1e68d62
Angel <araozangel842@gmail.com>
2025-05-02 22:44:53 +0100

    Deleting trash code

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,9 +1,7 @@
-import { View, StyleSheet, Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
-import { useEffect, useState } from "react";
 import Navbar from "@/components/navbar/Navbar";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import CustomView from "@/components/customs/CustomView";
 
 export default function TabTwoScreen() {
@@ -15,25 +13,6 @@ export default function TabTwoScreen() {
     { id: 8, url: "/fifth", text: "5°" },
   ];
 
-  //TODO: Check the use of this function
-  const checkAsyncStorage = async () => {
-    try {
-      const value = await AsyncStorage.getItem("user");
-      const timer = await AsyncStorage.getItem("loginTime");
-
-      if (value !== null && timer !== null) {
-        console.log("User is logged in");
-        console.log("User data:", JSON.parse(value));
-        console.log("Login time:", timer);
-      }
-    } catch (e) {
-      console.error("Error fetching users:", e);
-    }
-  };
-
-  useEffect(() => {
-    checkAsyncStorage();
-  }, []);
   return (
     <CustomView>
       <Navbar links={links} />
